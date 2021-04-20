af07c18983284f070c49c36840d77af7291efd1b
Yigit <yigit8@live.nl>
2021-04-20 11:35:12 +0200

    updatoooooooo

--- connect.php
+++ connect.php
@@ -1,3 +1,19 @@
 <?php
+    session_start();
+
+    $charset = "utf8mb4";
+    $username = "root";
+    $password = "eRLSTjCnW4Xd28g";
+    $database = "mysql:host=localhost;dbname=teamuitje;chars=$charset";
     
+
+
+    try {
+        $connect = new PDO($database, $username, $password);
+        $connect -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e) {
+        echo $e->getMessage();
+        die("Sorry, database probleem");
+    }
 ?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+session_start();
+include_once("includes/connect.php");
+$connect = $_SESSION['connect'];
+
+
+?>
